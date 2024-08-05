8c2dd18e13b4dc4fe2b2f1d39f6f1cd71944a10a
Kaillane <kaillanemartins18@gmail.com>
2024-08-04 22:25:33 -0300

    ajustando e construindo executável

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,23 +1,37 @@
 package main;
 
-import util.Controlador;
+import java.util.StringTokenizer;
 import util.LeituraArquivo;
+import util.Trie;
+import util.Controlador;
 
 public class Main {
     
 	public static void main(String[] args) {
         
         LeituraArquivo palavras = new LeituraArquivo("src\\entradas\\listadepalavras.txt");
-        LeituraArquivo dados = new LeituraArquivo("src\\entradas\\palavra.txt");
-        Controlador ctrl = new Controlador();
-        String palavra = dados.lerEnesima(0).toString();
-
-        System.out.println("Identificador de senhas fracas");
-        System.out.println("----------Iniciando----------");
-        System.out.println("");
+        //link da lista: https://github.com/0xrdf/Brazilian-wordlist/blob/master/brazilPasswords.txt
         
-         ctrl.adicionarDados(dados.lerTudo());
-         ctrl.tratar("buscar " + palavra);
+        LeituraArquivo dados = new LeituraArquivo("src\\entradas\\palavra.txt");
+        String palavra = dados.lerEnesima(0).toString();
+        Controlador ctrl = new Controlador();
+        Trie arv = new Trie();
+        
+        //inserindo palavras na árvore
+        ctrl.adicionarDados(palavras.lerTudo(), arv);
+        
+        System.out.println("Atividade 05 - Árvore Trie");
+        System.out.println("Dupla: Kaillane e Valter");
+        System.out.println("----------Iniciando----------");
+        System.out.println("Identificador de senhas fracas");
+        System.out.println("Para testar sua senha, insira ela no arquivo palavra.txt na pasta entradas\n");
+  
+        if(arv.contem(palavra) == false){
+            System.out.println("A senha " + palavra + " provavelmente é segura");
+        }else if(arv.contem(palavra) == true){
+            System.err.println("A senha " + palavra + " não é segura!!!");
+            System.out.println("Tente outra senha.");
+        }
         
         
       }
--- src/util/Controlador.java
+++ src/util/Controlador.java
@@ -1,69 +1,11 @@
 package util;
 
-import util.Trie;
-import java.util.StringTokenizer;
-
 public class Controlador {
-   private StringTokenizer comando;
-    private Trie arvTrie;
-    
-    public Controlador(){
-        arvTrie = new Trie();
-        
+
+    public Controlador() {
     }
-
-    public void tratar(String comando){
-
-        this.comando = new StringTokenizer(comando, " ");
-        String metodo = this.comando.nextToken().toLowerCase();
-
-
-        if(arvTrie != null){
-
-            //Escolher método
-            if(metodo.equals("inserir")){
-                if(this.comando.hasMoreElements()){
-                    Object dado = this.comando.nextToken("\n").substring(1);
-                    arvTrie.inserir((String) dado);
-                    System.out.println(dado + " adicionado");
-                }
-            }
-            else if(metodo.equals("remover")){
-                if(this.comando.hasMoreElements()){
-                    Object dado = this.comando.nextToken("\n").substring(1);
-                    arvTrie.remover((String) dado);
-                    System.out.println(dado + " removido");
-                }
-                
-            }
-            else if(metodo.equals("imprimir")){       
-                System.out.println("\n------IMPRIMIR------");
-                arvTrie.imprimir(); 
-                System.out.println("----------------------\n");
-            }
-            else if(metodo.equals("buscar")){
-                if(this.comando.hasMoreElements()){
-                    Object dado = this.comando.nextToken("\n").substring(1);
     
-                    if(arvTrie.contem((String) dado) == false){
-                        System.out.println(dado + " é uma senha segura");
-                    }
-                    else{
-                        System.err.println(dado + " não é uma senha segura");
-                    }
-                }else{
-                    System.out.println("Dado inválido");
-                }
-                
-            }
-            else{
-                System.out.println("Método inválido.");
-            }
-        
-        }
-    }
-
-    public void adicionarDados(Object[] dados){
+    public void adicionarDados(Object[] dados, Trie arvTrie){
         if(dados != null){
             for (int i = 0; i < dados.length ; i++){
               arvTrie.inserir((String) dados[i]);
@@ -71,3 +13,4 @@ public class Controlador {
         }   
     } 
 }
+
